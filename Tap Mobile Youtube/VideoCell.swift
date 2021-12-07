b02413094e2f6e0a4ed53ad8f0a396bc9dca36b0
softdev8 <oncemoreforyou@outlook.com>
2021-12-07 19:13:54 +0200

    Added loading indicator to video player view

--- Tap Mobile Youtube/VideoCell.swift
+++ Tap Mobile Youtube/VideoCell.swift
@@ -6,20 +6,30 @@
 
 import UIKit
 import youtube_ios_player_helper
+import MBProgressHUD
 
 class VideoCell: UITableViewCell {
     
     @IBOutlet weak var playerView: YTPlayerView!
+    var progressHUD: MBProgressHUD? = nil
     
     var videoRenderer: VideoRenderer? {
         didSet {
-            playerView.load(withVideoId: videoRenderer!.videoId)
+            if let videoId = videoRenderer?.videoId, videoId != oldValue?.videoId {
+                if progressHUD != nil {
+                    progressHUD?.hide(animated: false)
+                    progressHUD?.removeFromSuperview()
+                }
+                progressHUD = MBProgressHUD.showAdded(to: self.contentView, animated: true)
+                playerView.load(withVideoId: videoId)
+            }
         }
     }
 
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
+        
+        playerView.delegate = self
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
@@ -28,4 +38,13 @@ class VideoCell: UITableViewCell {
         // Configure the view for the selected state
     }
 
+}
+
+extension VideoCell: YTPlayerViewDelegate {
+    
+    func playerViewDidBecomeReady(_ playerView: YTPlayerView) {
+        progressHUD?.hide(animated: true)
+        progressHUD?.removeFromSuperview()
+        progressHUD = nil
+    }
 }
